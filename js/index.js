23e7ca8bda5df027aa1e93b123eb0ec40f4de672
Viacheslav <5arhipoff5@mail.ru>
2022-02-09 18:46:41 +0600

    add .

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
+//Поиск
     document.querySelector('.header__search').addEventListener('click', function() {
         document.querySelector('.header__form').classList.toggle('active')
       })
@@ -6,5 +7,192 @@ window.addEventListener('DOMContentLoaded', function() {
       document.querySelector('.header__search').addEventListener('click', function() {
         document.querySelector('.header__search').classList.toggle('btn-active')
       })
+//Кнопка паузы "Ранее"
+      document.querySelector('.header__music-btn-after').addEventListener('click', function() {
+        document.querySelector('.header__music-btn-after').classList.toggle('paused')
+      })
 
+      document.querySelector('.header__music-btn-after').addEventListener('click', function() {
+        document.querySelector('.header__paused').classList.toggle('paused')
+      })
+
+      document.querySelector('.header__paused').addEventListener('click', function() {
+        document.querySelector('.header__music-btn-after').classList.remove('paused')
+      })
+
+      document.querySelector('.header__paused').addEventListener('click', function() {
+        document.querySelector('.header__paused').classList.remove('paused')
+      })
+//Кнопка паузы сейчас
+      document.querySelector('.header__music-btn-before').addEventListener('click', function() {
+        document.querySelector('.header__music-btn-before').classList.toggle('paused')
+      })
+
+      document.querySelector('.header__music-btn-before').addEventListener('click', function() {
+        document.querySelector('.header__paused-left').classList.toggle('paused')
+      })
+
+      document.querySelector('.header__paused-left').addEventListener('click', function() {
+        document.querySelector('.header__music-btn-before').classList.remove('paused')
+      })
+
+      document.querySelector('.header__paused-left').addEventListener('click', function() {
+        document.querySelector('.header__paused-left').classList.remove('paused')
+      })
+
+//Кнопка паузы в подкастах
+
+//Europe
+
+document.querySelector('.podcast__btn-block').addEventListener('click', function() {
+  document.querySelector('.podcast__btn-block').classList.toggle('btn-paused')
 })
+
+document.querySelector('.podcast__btn-block').addEventListener('click', function() {
+  document.querySelector('.podcast__btn-pause').classList.toggle('btn-paused')
+})
+
+document.querySelector('.podcast__btn-pause').addEventListener('click', function() {
+  document.querySelector('.podcast__btn-pause').classList.remove('btn-paused')
+})
+
+document.querySelector('.podcast__btn-pause').addEventListener('click', function() {
+  document.querySelector('.podcast__btn-block').classList.remove('btn-paused')
+})
+
+//the music
+
+document.querySelector('.music').addEventListener('click', function() {
+  document.querySelector('.music').classList.toggle('btn-paused')
+})
+
+document.querySelector('.music').addEventListener('click', function() {
+  document.querySelector('.music-paused').classList.toggle('btn-paused')
+})
+
+document.querySelector('.music-paused').addEventListener('click', function() {
+  document.querySelector('.music').classList.remove('btn-paused')
+})
+
+document.querySelector('.music-paused').addEventListener('click', function() {
+  document.querySelector('.music-paused').classList.remove('btn-paused')
+})
+
+//people
+
+document.querySelector('.people').addEventListener('click', function() {
+  document.querySelector('.people').classList.toggle('btn-paused')
+})
+
+document.querySelector('.people').addEventListener('click', function() {
+  document.querySelector('.people-paused').classList.toggle('btn-paused')
+})
+
+document.querySelector('.people-paused').addEventListener('click', function() {
+  document.querySelector('.people').classList.remove('btn-paused')
+})
+
+document.querySelector('.people-paused').addEventListener('click', function() {
+  document.querySelector('.people-paused').classList.remove('btn-paused')
+})
+
+//africa
+
+document.querySelector('.africa').addEventListener('click', function() {
+  document.querySelector('.africa').classList.toggle('btn-paused')
+})
+
+document.querySelector('.africa').addEventListener('click', function() {
+  document.querySelector('.africa-paused').classList.toggle('btn-paused')
+})
+
+document.querySelector('.africa-paused').addEventListener('click', function() {
+  document.querySelector('.africa').classList.remove('btn-paused')
+})
+
+document.querySelector('.africa-paused').addEventListener('click', function() {
+  document.querySelector('.africa-paused').classList.remove('btn-paused')
+})
+
+//all
+
+document.querySelector('.all').addEventListener('click', function() {
+  document.querySelector('.all').classList.toggle('btn-paused')
+})
+
+document.querySelector('.all').addEventListener('click', function() {
+  document.querySelector('.all-paused').classList.toggle('btn-paused')
+})
+
+document.querySelector('.all-paused').addEventListener('click', function() {
+  document.querySelector('.all').classList.remove('btn-paused')
+})
+
+document.querySelector('.all-paused').addEventListener('click', function() {
+  document.querySelector('.all-paused').classList.remove('btn-paused')
+})
+
+//phisic
+
+document.querySelector('.phisic').addEventListener('click', function() {
+  document.querySelector('.phisic').classList.toggle('btn-paused')
+})
+
+document.querySelector('.phisic').addEventListener('click', function() {
+  document.querySelector('.phisic-paused').classList.toggle('btn-paused')
+})
+
+document.querySelector('.phisic-paused').addEventListener('click', function() {
+  document.querySelector('.phisic').classList.remove('btn-paused')
+})
+
+document.querySelector('.phisic-paused').addEventListener('click', function() {
+  document.querySelector('.phisic-paused').classList.remove('btn-paused')
+})
+
+//sound
+
+document.querySelector('.sound').addEventListener('click', function() {
+  document.querySelector('.sound').classList.toggle('btn-paused')
+})
+
+document.querySelector('.sound').addEventListener('click', function() {
+  document.querySelector('.sound-paused').classList.toggle('btn-paused')
+})
+
+document.querySelector('.sound-paused').addEventListener('click', function() {
+  document.querySelector('.sound').classList.remove('btn-paused')
+})
+
+document.querySelector('.sound-paused').addEventListener('click', function() {
+  document.querySelector('.sound-paused').classList.remove('btn-paused')
+})
+
+//etiquette
+
+document.querySelector('.etiquette').addEventListener('click', function() {
+  document.querySelector('.etiquette').classList.toggle('btn-paused')
+})
+
+document.querySelector('.etiquette').addEventListener('click', function() {
+  document.querySelector('.etiquette-paused').classList.toggle('btn-paused')
+})
+
+document.querySelector('.etiquette-paused').addEventListener('click', function() {
+  document.querySelector('.etiquette').classList.remove('btn-paused')
+})
+
+document.querySelector('.etiquette-paused').addEventListener('click', function() {
+  document.querySelector('.etiquette-paused').classList.remove('btn-paused')
+})
+
+});
+
+// Select
+
+const element = document.querySelector('.programs__select');
+const choices = new Choices(element, {
+  searchEnabled: false ,
+  itemSelectText: '' 
+  
+});
